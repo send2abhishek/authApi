4d8e5cdf02ed741ffeb082f1d5ac0f9e4c1a952d
Abhishek Aryan <send2abhishek@live.com>
2020-06-06 13:48:30 +0530

    modified routes page

--- Routes/appRoutes.js
+++ Routes/appRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const route = express.Router();
 const controller = require("../Controllers/appController");
 const validateUsers = require("../Middlewares/usersValidate");
-const AuthCheck = require("../middlewares/authvalidate");
+const AuthCheck = require("../Middlewares/authvalidate");
 // all routes
 route.post("/login", validateUsers.validateUser, controller.login);
 route.post(
